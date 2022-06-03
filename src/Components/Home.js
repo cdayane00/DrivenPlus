00400827c6d4457e43742765f00e388ca36d1339
Carla <carla.dayane.berredo@gmail.com>
2022-06-03 16:34:21 -0400

    tela de assinar um plano pronta

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,42 @@
+import styled from 'styled-components';
+import { useParams,useNavigate } from 'react-router-dom';
+import { useState, useEffect,useContext } from 'react';
+import UserContext from '../context/UserContext';
+import axios from 'axios';
+
 function Home(){
+    
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const {usuario} = useContext(UserContext);
+    const [plano, setPlano] = useState([]);
+
+    useEffect(()=>{
+        const URL = `https://mock-api.driven.com.br/api/v4/driven-plus/subscriptions/memberships/${id}`;
+        const config = {
+            headers: {
+                Authorization: `Bearer ${usuario.token}`,
+            },
+        };
+        const promise = axios.get(URL,config);
+        promise.then((response)=>{
+            setPlano(response.data);
+        });
+        promise.catch((erro)=>{
+            alert(erro.response.statusText);
+        });
+    },[]);
+
     return(
-        <h1>Sou home</h1>
+        <Fundo>
+            <img key={`plano ${id}`} src={plano.image}/>
+
+        </Fundo>
     )
 }
 export default Home;
+
+const Fundo = styled.div`
+    width: 375px;
+    height: 667px;
+    background: black;`;
--- src/Components/Plano.js
+++ src/Components/Plano.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 import { useState, useEffect,useContext } from 'react';
 import UserContext from '../context/UserContext';
 import axios from 'axios';
+import fechar from '../img/fechar.png'
+import voltar from '../img/voltar.png'
 
 function Plano(){
+    const navigate = useNavigate();
     const {id} = useParams();
     const {usuario} = useContext(UserContext);
-    const [plano,setPlano] = useState([]);
-
+    const [plano, setPlano] = useState([]);
+    const [dadosAssina, setDadosAssina] = useState({membershipId:"", cardName:"", cardNumber:"", securityNumber:"", expirationDate:""});
+    const [verificaModal, setVerificaModal] = useState(false);
 
     useEffect(()=>{
         const URL = `https://mock-api.driven.com.br/api/v4/driven-plus/subscriptions/memberships/${id}`;
@@ -26,19 +30,83 @@ function Plano(){
         });
     },[]);
 
+    function assinarPlano(event){
+        event.preventDefault();
+
+        const URL = "https://mock-api.driven.com.br/api/v4/driven-plus/subscriptions";
+        const config = {
+            headers: {
+                Authorization: `Bearer ${usuario.token}`,
+            },
+        };
+        
+        const promise = axios.post(URL, {
+            membershipId: dadosAssina.membershipId,
+            cardName: dadosAssina.cardName,
+            cardNumber: dadosAssina.cardNumber,
+            securityNumber: dadosAssina.securityNumber,
+            expirationDate: dadosAssina.expirationDate
+        }, config);
+        
+        promise.then(()=>{
+            console.log("sucesso");
+            modal();
+        });
+        
+        console.log(dadosAssina.cardName)
+        console.log(dadosAssina.membershipId)
+        console.log(usuario.token);
+        promise.catch(erro=>alert(erro.response.statusText));
+
+    }
+
+    function modal(){
+        setVerificaModal(true);
+    }
+    function vaiParaHome(){
+        navigate('/home');
+    }
+    function vaiParaPlanos(){
+        navigate('/subscriptions');
+    }
+
     return(
         <Fundo>
-            <div></div>
+            <Topo>
+                <img onClick={vaiParaPlanos}  className='div-voltar' src={voltar}/>
+                <div className='espaco'>
+                    {verificaModal ?(
+                        
+                        <img onClick={()=> setVerificaModal(false)} className='div-fecha' src={fechar}/>
+                    )
+                    : <></> }
+
+                </div>
+            </Topo>
+
+            {verificaModal ? (
+                <div className='fundo'>
+                    <div className='div-modal'>  
+                        <p className='p-modal'>Tem certeza que deseja assinar o plano {plano.name} (R$ {plano.price})?</p>  
+                        <button onClick={()=> setVerificaModal(false)} className='nao'>Não</button>
+                        <button onClick={vaiParaHome} className='sim'>Sim</button>
+                    </div>
+                </div>  
+            )
+            :
+                <></>  
+            }
+            
             <img key={`plano ${id}`} src={plano.image}/>
             <p>{plano.name}</p>
             <h3>Benefícios:</h3>
             <h3>Preço:</h3>
             <h4>R$ {plano.price} cobrados mensalmente</h4>
-            <CriaForm>
-                <input type="text" placeholder='Nome impresso no cartão'/>
-                <input type="text" placeholder='Digitos do cartão'/>
-                <input className='input-menor' type="text" placeholder='Código de segurança'/>
-                <input className='input-menor' type="text" placeholder='Validade'/>
+            <CriaForm onSubmit={assinarPlano}>
+                <input type="text" placeholder='Nome impresso no cartão' value={dadosAssina.cardName} required onChange={e=> setDadosAssina({...dadosAssina, membershipId: id, cardName: e.target.value})}/>
+                <input type="number" placeholder='Digitos do cartão' value={dadosAssina.cardNumber} required onChange={e=> setDadosAssina({...dadosAssina, cardNumber: e.target.value})}/>
+                <input className='input-menor' type="text" placeholder='Código de segurança' value={dadosAssina.securityNumber} required onChange={e=> setDadosAssina({...dadosAssina, securityNumber: e.target.value})}/>
+                <input className='input-menor' type="date" placeholder='Validade' value={dadosAssina.expirationDate} required onChange={e=> setDadosAssina({...dadosAssina, expirationDate: e.target.value})}/>
                 <button type='submit'>Assinar</button>
 
             </CriaForm>
@@ -49,6 +117,9 @@ function Plano(){
     )
 }
 export default Plano;
+
+const Topo = styled.div`
+    display:flex;`;
 
 const CriaForm = styled.form`
     display: flex;
@@ -90,7 +161,67 @@ const Fundo = styled.div`
     background: black;
     
 
-    div{
+    .div-modal{
+        width: 248px;
+        height: 210px;
+        background: #FFFFFF;
+        border-radius: 12px;
+        position: fixed;
+        margin-left: 50px;
+        margin-top: 180px;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .div-modal button{
+        width: 95px;
+        height: 52px;
+        margin-left: 18px;
+        border-radius: 8px;
+        border: 0;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        color: #FFFFFF;
+    }
+    .sim{
+        background: #FF4791;
+    }
+    .nao{
+        background: #CECECE;
+    }
+    .div-fecha{
+        margin-left: 250px;
+        margin-top: 20px;
+    }
+    .fundo{
+        width: 375px;
+        height: 667px;
+        background: rgba(0, 0, 0, 0.7);
+        position: fixed;
+    }
+    .div-voltar{
+        margin-left: 30px;
+        margin-top: 20px; 
+        height:30px;
+
+    }
+    .p-modal{
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 21px;
+        text-align: center;
+        color: #000000;
+        margin-left: 15px;
+        margin-right: 15px;
+        margin-top: 30px;
+    }
+    
+
+    .espaco{
         height: 100px;
     }
     img{
